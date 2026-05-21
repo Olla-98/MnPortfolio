fa72c4cefc0ea2d930f0be2d8724403f69129d64
Olla-98 <alsattoufola@gmail.com>
2026-05-21 15:47:03 +0200

    New project: Vue-js To-Do-app installerd

---
+++ vuejs/to-do-app/src/App.vue
@@ -0,0 +1,47 @@
+<script setup>
+
+</script>
+
+<template>
+  <header>
+    <!-- <img alt="Vue logo" class="logo" src="./assets/logo.svg" width="125" height="125" /> -->
+
+    <div class="wrapper">
+      <h1>To-Do app is mijn eerste project in Vue</h1>
+    </div>
+  </header>
+
+  <main>
+    
+  </main>
+</template>
+
+<style scoped>
+header {
+  line-height: 1.5;
+  color: var(--section-gap);
+}
+
+.logo {
+  display: block;
+  margin: 0 auto 2rem;
+}
+
+@media (min-width: 1024px) {
+  header {
+    display: flex;
+    place-items: center;
+    padding-right: calc(var(--section-gap) / 2);
+  }
+
+  .logo {
+    margin: 0 2rem 0 0;
+  }
+
+  header .wrapper {
+    display: flex;
+    place-items: flex-start;
+    flex-wrap: wrap;
+  }
+}
+</style>
